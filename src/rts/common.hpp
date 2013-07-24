5bf3d7e5206ed7bb5670d4d6302ba47021fa3761
Gwilym Kuiper <kuiper.gwilym@gmail.com>
2013-07-24 11:06:18 +0100

    I know its a bit extreme, but oh well

--- src/rts/common.hpp
+++ src/rts/common.hpp
@@ -1,13 +1,8 @@
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
-#ifndef NDEBUG
 #define assert(x) \
     do { if(!(x)) { rts::assert::fail(#x, __FILE__, __LINE__); } } while(__LINE__ == -1)
-#else
-#define assert(x) \
-    do { (x); } while(__LINE__ == -1)
-#endif
 
 namespace rts
 {
